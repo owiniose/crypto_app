f21f8d7f1b1bcef6859af2787d59d6f3adfbf0bd
Owini <owiniose@gmail.com>
2023-01-02 15:10:04 +0100

    commit

--- components/HomeHeader.js
+++ components/HomeHeader.js
@@ -31,6 +31,8 @@ const HomeHeader = () => {
         <Svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#c2cdd9" height={25} width={25} >
   <Path fill-rule="evenodd" d="M5.25 9a6.75 6.75 0 0113.5 0v.75c0 2.123.8 4.057 2.118 5.52a.75.75 0 01-.297 1.206c-1.544.57-3.16.99-4.831 1.243a3.75 3.75 0 11-7.48 0 24.585 24.585 0 01-4.831-1.244.75.75 0 01-.298-1.205A8.217 8.217 0 005.25 9.75V9zm4.502 8.9a2.25 2.25 0 104.496 0 25.057 25.057 0 01-4.496 0z" clip-rule="evenodd" />
 </Svg>
+<View style={styles.tabBadge}>
+</View>
 
         </View>
       </View>
@@ -67,6 +69,18 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         width:"100%",
        justifyContent:'space-between',
+    },
+    tabBadge: {
+      position: 'absolute',
+      top: -3,
+      right: -2,
+      backgroundColor: '#fa6666',
+      borderRadius: 15,
+      zIndex: 2,
+      height:13,
+      width:13,
+      borderColor:"black",
+      borderWidth:2
     },
 
 })
